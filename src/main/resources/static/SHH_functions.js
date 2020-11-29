a164b76aed7847e403e917c7a01e179b2e95b197
Shijun Deng <dengshijun233@gmail.com>
2020-11-29 15:14:40 -0500

    add the classes for room and SHH

--- src/main/resources/static/SHH_functions.js
+++ src/main/resources/static/SHH_functions.js
@@ -1,5 +1,6 @@
 class Zone{
-    constructor(rooms, periodicTempSetting){
+    constructor(zoneID, rooms, periodicTempSettings){
+        this.zoneID = zoneID;
         this.rooms = rooms;
         this.periodicTempSettings = periodicTempSettings;
     }
@@ -61,5 +62,52 @@ class PeriodicTempSetting {
         else{
             return false;
         }
+    }
+}
+
+class Room{
+    constructor(name, temperature){
+        this.name = name;
+        this.temperature = temperature;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getTemperature(){
+        return this.temperature;
+    }
+
+    // should we allow the user to set the name of a room?
+    setName(name){
+        this.name = name;
+    }
+
+    setTemperature(newTemp){
+        this.temperature = newTemp;
+    }
+}
+
+class SHH{
+    constructor(zones){
+        this.zones = zones;
+    }
+
+    getAllZones(){
+        return zones;
+    }
+
+    getZoneById(id){
+        this.forEach(zone => {
+            if (zone.zoneID == id) {
+                return zone;
+            }
+        });
+        alert("the zone is not found, please the ID.");
+    }
+
+    setZoneById(id, ){
+
     }
 }
