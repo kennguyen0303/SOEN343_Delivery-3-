0228804f070159f37758561265b36a8c835323bc
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-12-03 23:04:30 -0500

    Update continuousMonitoring.js

--- src/main/resources/static/javascript/continuousMonitoring.js
+++ src/main/resources/static/javascript/continuousMonitoring.js
@@ -12,7 +12,7 @@ class  HAVCController{
     constructor(newZone){
         this.zone = newZone;
         this.id = newZone.zoneID;
-        this.state = HAVCStates.states.STOPPED; // paused?
+        this.state = HAVCStates.states.PAUSED;
     }
 
     this.startMonitoring = function(){
@@ -28,45 +28,55 @@ class  HAVCController{
         var rooms = this.zone.getRooms();
         for(let i = 0; i< rooms.length; i++){
 
+            // check if windows have been closed since last temperature reading
             if(this.state == HAVCStates.states.STOPPED){
                  var index = rooms[i].window_index_array();
-                 if(false /*no windows open*/){ // TODO: find state of windows
+                 if( window_index_array[i].status == 'closed'){ // TODO: VERIFY THAT THIS IS ACCURATE. Do we only have 1 window per room?
                     this.state = HAVCStates.states.RUNNING;
                  }
             }
 
-            if(this.state == HAVCStates.states.RUNNING || this.state == HAVCStates.states.PAUSED){
+            // adjust temperature if the simulation is not stopped
+            if(!this.state == HAVCStates.states.STOPPED){
                 var temperatureInRoom = rooms[i].getTemperature();
                 var increase;
                 setHAVCState(idealTemperature, temperatureInRoom);
 
                 if(this.state == HAVCStates.states.RUNNING){
-                                increase = (idealTemperature > temperatureInRoom);
-                                updateRoomTemperature(increase, 0.1, room);
+                   increase = (idealTemperature > temperatureInRoom);
+                   updateRoomTemperature(increase, 0.1, room);
                 }
                 else if(this.state == HAVCStates.states.PAUSED){
-                                increase = (outsideTemperature > temperatureInRoom);
-                                updateRoomTemperature(increase, 0.05, room);
+                    increase = (outsideTemperature > temperatureInRoom);
+                    updateRoomTemperature(increase, 0.05, room);
                 }
             }
 
-            if(temperatureInRoom == 0){
-                 var consoleNode = document.createElement("p");
-                 var text = "Caution! Temperature below zero. Pipes may burst."
-                 var textNode =  document.createTextNode(text);
-                 consoleNode.appendChild(textNode);
-                 document.getElementById("outputConsole").appendChild(consoleNode);
-
-                 // write to output log
-                 writeToFile(text);
-             }
-
-            if(temperatureInRoom > outsideTemperature){
-                openWindowsInSummer(room);
-            }
+            monitorPipes(temperatureInRoom);
+            monitorWindows(temperatureInRoom, outsideTemperature);
         }
 
         setTimeout(monitorTemperature(), temperatureTimeout);
+    }
+
+    this.monitorPipes = function(temperatureInRoom){
+        if(temperatureInRoom == 0){
+            // output to console
+            var consoleNode = document.createElement("p");
+            var text = "Caution! Temperature below zero. Pipes may burst."
+            var textNode =  document.createTextNode(text);
+            consoleNode.appendChild(textNode);
+            document.getElementById("outputConsole").appendChild(consoleNode);
+
+           // write to output log
+           writeToFile(text);
+        }
+    }
+
+    this.monitorWindows = function(temperatureInRoom, outsideTemperature){
+        if(temperatureInRoom > outsideTemperature){
+            openWindowsInSummer(room);
+        }
     }
 
     this.setHAVCState = function(idealTemperature, temperatureInRoom)
@@ -92,12 +102,12 @@ class  HAVCController{
          }
      }
 
-     this.updateRoomTemperature = function(increase, rate, room){
-            var currentTemp =  room.getTemperature();
+    this.updateRoomTemperature = function(increase, rate, room){
+        var currentTemp =  room.getTemperature();
 
-            if(!this.state == HAVCStates.states.PAUSED || !currentTemp == outsideTemp){
-                room.temperature = increase ? (currentTemp + rate): (currentTemp - rate);
-            }
-     }
+       if(!this.state == HAVCStates.states.PAUSED || !currentTemp == outsideTemp){
+          room.temperature = increase ? (currentTemp + rate): (currentTemp - rate);
+       }
+    }
 }
 
