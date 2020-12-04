ab93e39ed2c7e773d5810223b71a38e0027f62e2
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-12-03 21:09:38 -0500

    Update continuousMonitoring.js

--- src/main/resources/static/javascript/continuousMonitoring.js
+++ src/main/resources/static/javascript/continuousMonitoring.js
@@ -1,43 +1,48 @@
+// enum for HAVC States
 class HAVCStates{
     static const states = {
       PAUSED: 'paused',
       RUNNING: 'running',
-      STOPPED: 'stopped'
+      STOPPED: 'stopped' // might be redundant
     }
 }
 
 class  HAVCController{
 
     constructor(newZone){
-        this.zone = Zone;
+        this.zone = newZone;
         this.id = newZone.zoneID;
-        this.state = HAVCStates.states.STOPPED;
+        this.state = HAVCStates.states.STOPPED; // paused?
     }
 
-    function startMonitoring(){
+    this.startMonitoring = function(){
         monitorTemperature();
     }
 
-    function monitorTemperature(){
-        var outsideTemperature = 18; /*getOutsideTemperature();*/
+    this.monitorTemperature = function(){
+        var outsideTemperature = SHH.outdoorTemp; //TODO: Get outside temperature
 
+        // TODO: Get ideal temperature in a zone
         var idealTemperature;
 
         var rooms = this.zone.getRooms();
         for(let i = 0; i< rooms.length; i++){
-            var temperatureInRoom = rooms[i].temperature;
-            var increase = (idealTemperature > temperatureInRoom);
+            var temperatureInRoom = rooms[i].getTemperature();
+            var increase;
             setHAVCState(idealTemperature, temperatureInRoom);
 
             if(this.state == HAVCStates.states.RUNNING){
+                increase = (idealTemperature > temperatureInRoom);
                 updateRoomTemperature(increase, 0.1, room);
             }
             else if(this.state == HAVCStates.states.PAUSED){
+                increase = (outsideTemperature > temperatureInRoom);
                 updateRoomTemperature(increase, 0.05, room);
             }
 
             if(temperatureInRoom == 0){
-                // alert console
+                // TODO: send to application console and log file
+                alert("Get your house temperature under control plz thx");
             }
 
             if(temperatureInRoom > outsideTemperature){
@@ -45,11 +50,10 @@ class  HAVCController{
             }
         }
 
-        // TODO: figure how to get these temperatures
         setTimeout(monitorTemperature(), temperatureTimeout);
     }
 
-    function setHAVCState(idealTemperature, temperatureInRoom)
+    this.setHAVCState = function(idealTemperature, temperatureInRoom)
     {
         if(Math.abs(idealTemperature - temperatureInRoom) > 1){
             this.state = HAVCStates.states.RUNNING;
@@ -62,18 +66,22 @@ class  HAVCController{
         }
     }
 
-     function openWindowsInSummer(room){
+    this.openWindowsInSummer = function(room){
          var currentSeason = getCurrentSeason();
          if(currentSeason == Seasons.season.SUMMER){
              this.state = HAVCStates.states.STOPPED;
-             room.openWindow(); // do I need to check for away mode???
+
+             // do I need to check for away modes?
+             room.openWindow(); // no parameters opens all windows in room
          }
      }
 
-     function updateRoomTemperature(increase, rate, room){
+     this.updateRoomTemperature = function(increase, rate, room){
             var currentTemp =  room.getTemperature();
-            var roomTemp = increase ? currentTemp + rate: currentTemp - rate;
-            room.temperature = roomTemp;
+
+            if(!this.state == HAVCStates.states.PAUSED || !currentTemp == outsideTemp){
+                room.temperature = increase ? (currentTemp + rate): (currentTemp - rate);
+            }
      }
 }
 
